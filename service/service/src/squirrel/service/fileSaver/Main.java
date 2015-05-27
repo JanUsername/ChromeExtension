777356f2b32b2c4be38466a3acc0d675114eba30
JanUsername <janr.schmid.niederkofler@gmail.com>
2015-05-27 13:55:56 +0200

    creates threads without killing them

--- service/service/src/squirrel/service/fileSaver/Main.java
+++ service/service/src/squirrel/service/fileSaver/Main.java
@@ -1,6 +1,7 @@
 package squirrel.service.fileSaver;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -14,7 +15,7 @@ import java.util.concurrent.Executors;
 
 public class Main {
 
-	private static final int numberOfThreads = 100;
+//	private static final int numberOfThreads = 100;
 	private static final int port = 8195;
 	private static final Executor threadPool = Executors
 			.newFixedThreadPool(numberOfThreads);
@@ -28,8 +29,6 @@ public class Main {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		String uuid;
-		;
 		ServerSocket socket = null;
 		try {
 			socket = new ServerSocket(port);
@@ -38,6 +37,7 @@ public class Main {
 				Runnable task = new Runnable() {
 					@Override
 					public void run() {
+						System.out.println("got a request to handle");
 						HandleRequest(connection);
 					}
 				};
@@ -56,52 +56,75 @@ public class Main {
 	private static void HandleRequest(Socket socket) {
 		String urlFilter = "/?url";
 		BufferedReader in;
-		// PrintWriter out;
+		String homeFolder;
+		String squirrelFolderPath;
+		String outboxFolderPath;
+//		 PrintWriter out;
 		try {
 			in = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
-
+			PrintWriter writer = null;
 			/**
 			 * reads the input line for line
 			 */
+			System.out.println("recieved input");
 			for (String thisLine = in.readLine(); thisLine != null; thisLine = in
 					.readLine()) {
-				System.out.println(thisLine);
-
 				/**
 				 * filters the url and save it into a json
 				 */
 				if (thisLine.contains(urlFilter)) {
-
+					
+					//decodes the url
 					String url = URLDecoder.decode(thisLine.substring(
 							thisLine.indexOf('?') + 4, thisLine.length() - 9),
 							"UTF-8");
-					System.out.println("Time: " + new Date().getTime());
+					System.out.println("found url:" + url);
+					
+					//variables for creating the json
 					String uuid = UUID.randomUUID().toString();
-					String date = new Date().toString();
+					long date = new Date().getTime();
 					String jSonObject;
 					String jSonURL = "\"url\": \"" + url + "\", ";
-					String jSonUuid = "\"uuid\": \"" + uuid + "\", ";
 					String jSonDate = "\"date\": \"" + date + "\"";
-					jSonObject = "{ " + jSonURL + jSonUuid + jSonDate + " }";
-					PrintWriter writer = new PrintWriter(
-							"G:/Uni/Internship/Project/test.json", "UTF-8");
-					// todo delete if not needed anymore
-					writer.println(jSonObject);
-					writer.close();
-					System.out.println(jSonObject);
+					jSonObject = "{ " + jSonURL +  jSonDate + " }";
+					
+					//create the printwirter and what you need to save the json
+					
+					homeFolder = System.getProperty("user.home");
+					squirrelFolderPath = homeFolder + "\\Squirrel";
+					outboxFolderPath = squirrelFolderPath + "\\Outbox";
+					File folderSquirrel = new File(squirrelFolderPath);
+					File folderOutbox = new File(outboxFolderPath);
+					String jsonPath = outboxFolderPath + "\\" + uuid+ ".json";
+					
+					System.out.println(jsonPath);
+					
+					//write the json, and creates the folder if they do not exist
+					if (folderOutbox.exists()) {
+						// save json here
+						writer = new PrintWriter(jsonPath, "UTF-8");
+					} else if (folderSquirrel.exists()) {
+						new File(outboxFolderPath).mkdirs();
+						writer = new PrintWriter(jsonPath, "UTF-8");
+					} else {
+						new File(outboxFolderPath).mkdirs();
+						writer = new PrintWriter(jsonPath, "UTF-8");
+					}
 
-				}
+					writer.println(jSonObject);
+					writer.flush();
+					}
 			}
-			/*
-			 * out = new PrintWriter(socket.getOutputStream(), true);
-			 * out.println("HTTP/1.0 200");
-			 * out.println("Content-type: text/html");
-			 * out.println("Server-name: squirrel"); String response =
-			 * "<html><head></head></html>"; out.println("Content-length: " +
-			 * response.length()); out.println(""); out.println(response);
-			 * out.flush(); out.close();
-			 */
+			
+//			  out = new PrintWriter(socket.getOutputStream(), true);
+//			  out.println("HTTP/1.0 200");
+//			  out.println("Content-type: text/html");
+//			  out.println("Server-name: squirrel"); String response =
+//			  "<html><head></head></html>"; out.println("Content-length: " +
+//			  response.length()); out.println(""); out.println(response);
+//			  out.flush(); out.close();
+			  writer.close();
 			socket.close();
 			in.close();
 		} catch (IOException e) {
@@ -109,6 +132,7 @@ public class Main {
 		} finally {
 			if (socket != null) {
 				try {
+					System.out.println("this socket is closed now");
 					socket.close();
 				} catch (IOException e) {
 					e.printStackTrace();
